a3b36b5f07d65d63e9eb9cdd7efc852f75876f6f
Jorge <jorge@Jorges-MacBook-Pro.local>
2022-11-10 00:43:47 -0300

    first commit

---
+++ Player/Player.gd
@@ -0,0 +1,42 @@
+extends KinematicBody2D
+
+const SPEED = 4
+const MAX_SPEED = 200
+const FRICTION = 0.05
+
+
+var motion = Vector2()
+
+func _physics_process(delta):
+	update_movement()
+	move_and_slide(motion)
+	
+func update_movement():
+	# look_at(get_global_mouse_position())
+	if Input.is_action_pressed("move_down") and not Input.is_action_pressed("move_up"):
+		motion.y = clamp(motion.y + SPEED, 0, MAX_SPEED)
+	elif Input.is_action_pressed("move_up") and not Input.is_action_pressed("move_down"):
+		motion.y = clamp(motion.y - SPEED, -MAX_SPEED, 0)
+	else:
+		motion.y = lerp(motion.y, 0, FRICTION)
+		
+	if Input.is_action_pressed("move_left") and not Input.is_action_pressed("move_right"):
+		motion.x = clamp(motion.x - SPEED, -MAX_SPEED, 0)
+	elif Input.is_action_pressed("move_right") and not Input.is_action_pressed("move_left"):
+		motion.x = clamp(motion.x + SPEED, 0, MAX_SPEED)
+	else:
+		motion.x = lerp(motion.x, 0, FRICTION)
+
+func _ready():
+	pass # Replace with function body.
+
+func fire():
+	$Sprite/RayCast2D.add_child(load("res://Player/Fire.tscn").instance())
+
+func _input(event):
+	if Input.is_action_just_pressed("fire"):
+		fire()
+		
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
